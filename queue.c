fec38d8a8854c9155603eedab13403228f9ae151
nienzu <ibqqz0602@gmail.com>
2018-10-04 09:45:20 +0800

    Finish the task & Pass the queue test

--- queue.c
+++ queue.c
@@ -26,7 +26,13 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
+    if (q == NULL) {
+        printf("No more memory for queue\n");
+        return NULL;
+    }
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
@@ -35,6 +41,19 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
+    if (q == NULL) {
+        return;
+    } else if (q->head == NULL) {
+        free(q);
+        return;
+    }
+    list_ele_t *tmp;
+    while (q->head != NULL) {
+        tmp = q->head;
+        q->head = q->head->next;
+        free(tmp->value);
+        free(tmp);
+    }
     free(q);
 }
 
@@ -48,12 +67,40 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
+    if (q == NULL) {
+        return false;
+    }
     /* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL) {
+        printf("malloc return NULL when allocate new head\n");
+        return false;
+    }
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
-    newh->next = q->head;
-    q->head = newh;
+
+    newh->value = malloc(strlen(s) * sizeof(char) + 1);
+
+    if (newh->value == NULL) {
+        printf("malloc return NULL when allocate new value\n");
+        free(newh);
+        return false;
+    }
+
+    strcpy(newh->value, s);
+
+    /* Assign tail */
+    if (q->head == NULL) {
+        newh->next = NULL;
+        q->head = newh;
+    } else {
+        newh->next = q->head;
+        q->head = newh;
+    }
+    if (q->tail == NULL) {
+        q->tail = q->head;
+    }
+    q->size++;
     return true;
 }
 
@@ -69,7 +116,33 @@ bool q_insert_tail(queue_t *q, char *s)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    if (q == NULL) {
+        return false;
+    }
+    list_ele_t *newt;
+    newt = malloc(sizeof(list_ele_t));
+    if (newt == NULL) {
+        printf("malloc return NULL when allocate new head\n");
+        return false;
+    }
+    newt->value = malloc(strlen(s) * sizeof(char) + 1);
+    if (newt->value == NULL) {
+        printf("malloc return NULL when allocate new value \n");
+        free(newt);
+        return false;
+    }
+    strcpy(newt->value, s);
+    newt->next = NULL;
+
+    if (q->tail != NULL) {
+        q->tail->next = newt;
+        q->tail = newt;
+    } else {
+        q->tail = newt;
+        q->head = newt;
+    }
+    q->size++;
+    return true;
 }
 
 /*
@@ -83,7 +156,24 @@ bool q_insert_tail(queue_t *q, char *s)
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* You need to fix up this code. */
-    q->head = q->head->next;
+    if (q == NULL || q->head == NULL) {
+        return false;
+    }
+    if (sp != NULL) {
+        strncpy(sp, q->head->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    list_ele_t *tmp = q->head;
+    if (q->head->next == NULL) {
+        q->head = NULL;
+        q->tail = NULL;
+    } else {
+        q->head = q->head->next;
+    }
+    q->size--;
+    free(tmp->value);
+    free(tmp);
     return true;
 }
 
@@ -95,7 +185,11 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if (q == NULL || q->head == NULL) {
+        return 0;
+    } else {
+        return q->size;
+    }
 }
 
 /*
@@ -108,4 +202,24 @@ int q_size(queue_t *q)
 void q_reverse(queue_t *q)
 {
     /* You need to write the code for this function */
+    if (q == NULL || q->head == NULL) {
+        return;
+    }
+    list_ele_t *tmp = NULL;
+    list_ele_t *newh = NULL;
+    q->tail = q->head;
+    while (q->head != NULL) {
+        if (tmp == NULL) {
+            tmp = q->head;
+            q->head = q->head->next;
+            tmp->next = NULL;
+            newh = tmp;
+        } else {
+            tmp = q->head;
+            q->head = q->head->next;
+            tmp->next = newh;
+            newh = tmp;
+        }
+    }
+    q->head = newh;
 }
